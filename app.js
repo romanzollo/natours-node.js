b852dfe57e6f79cb4e2420a934ca7e75c9656627
Roman Zlagodukhin <zloyandmad@yandex.ru>
2025-07-17 11:20:34 +0200

    handling POST requests

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
+
+// этот middleware позволяет работать с JSON в теле запросов
+app.use(express.json());
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -14,6 +18,29 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // req.body — здесь будут данные из JSON
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = { ...req.body, id: newId };
+  //   const newTour = Object.assign({}, req.body, { id: newId }); -  альтернативный вариант
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tours: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;

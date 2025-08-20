eb5009bb02ef51cfd1f8f0ba6989558d25576115
Roman Zlagodukhin <zloyandmad@yandex.ru>
2025-08-20 22:38:53 +0200

    feat(api): add simple pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -41,6 +41,19 @@ const getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // --- ПАГИНАЦИЯ --- //
+    const page = Number(req.query.page) || 1; // по умолчанию первая страница
+    const limit = Number(req.query.limit) || 10; // по умолчанию 10 документов на странице
+    const skip = (page - 1) * limit; // сдвигаемся на 1 страницу и умножаем на количество документов на странице
+
+    // page=2&limit=10, 1-10 page 1, 11-20 page 2, 21-30 page 3, ...
+    query = query.skip(skip).limit(limit); // skip - встроенный метод в mongoose
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments(); // countDocuments - встроенный метод в mongoose
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     // ВЫПОЛНЯЕМ ЗАПРОС
     const tours = await query;
 

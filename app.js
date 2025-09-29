6b39cc06a866e3495510a3ee454fdc59b25bd6cc
Roman Zlagodukhin <zloyandmad@yandex.ru>
2025-09-29 22:34:02 +0200

    feat(router): catch all unmatched routes and return 404

--- app.js
+++ app.js
@@ -43,4 +43,13 @@ app.use('/api/v1/tours', tourRouter);
 // Например: GET /api/v1/users, DELETE /api/v1/users/5
 app.use('/api/v1/users', userRouter);
 
+// Маршрут для несуществующих маршрутов
+// /(.*)/ - любой маршрут (так как Express 5 перешёл на router 2.0 и path-to-regexp 8.0)
+app.all(/(.*)/, (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`
+  });
+});
+
 module.exports = app;

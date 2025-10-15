372bf52e3571b6e218fcfe3dd2a8c3591dea6d6c
Roman Zlagodukhin <zloyandmad@yandex.ru>
2025-10-15 15:37:09 +0200

    fix(auth): fix create a new user by singup function

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,12 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 const singup = catchAsync(async (req, res) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm
+  });
 
   res.status(201).json({
     status: 'success',

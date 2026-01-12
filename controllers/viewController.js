d5f178989e66d2317fea92f8385f04035e58b686
Roman Zlagodukhin <zloyandmad@yandex.ru>
2026-01-12 22:09:24 +0200

    feat(tours): create overview page template (PUG)

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,19 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Получаем все данные из коллекции Tours
+  const tours = await Tour.find();
+
+  // 2) Создаем шаблон
+
+  // 3) Рендерим шаблон с данными из 1го шага
+
   res.status(200).render('overview', {
-    title: 'All Tours'
+    title: 'All Tours',
+    tours: tours
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
